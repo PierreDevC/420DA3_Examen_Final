bf322e3a118b71631e945968b890940af1d261a1
P-S.Cypre <pscypre@gmail.com>
2024-12-11 09:04:38 -0500

    Étape 4: Développement de classes d’accès aux données 💾

---
+++ 420DA3_Examen_Final/DataAccess/CoursDAO.cs
@@ -0,0 +1,40 @@
+﻿using _420DA3_Examen_Final.Business.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.DataAccess;
+internal class CoursDAO
+{
+    private AppDbContext context;
+
+    public CoursDAO(AppDbContext context)
+    {
+        this.context = context;
+    }
+
+    public List<Cours> GetAll()
+    {
+        return this.context.Courses
+            .ToList();
+    }
+
+    public List<Cours> GetEtudiantCoursInscrits (Etudiant etudiant)
+    {
+        return this.context.Courses
+            .Include(c => c.EtudiantInscrits)
+            .Where(c => c.EtudiantInscrits.Contains(etudiant))
+            .ToList();
+    }
+
+    public List<Cours> GetEtudiantNotCoursInscrits(Etudiant etudiant)
+    {
+        return this.context.Courses
+            .Include(c => c.EtudiantInscrits)
+            .Where(c => !c.EtudiantInscrits.Contains(etudiant))
+            .ToList();
+    }
+}
---
+++ 420DA3_Examen_Final/DataAccess/EtudiantDAO.cs
@@ -0,0 +1,56 @@
+﻿using _420DA3_Examen_Final.Business.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.DataAccess;
+internal class EtudiantDAO
+{
+    private AppDbContext context;
+
+    public EtudiantDAO(AppDbContext context)
+    {
+        this.context = context;
+    }
+
+    public List<Etudiant> GetAll()
+    {
+        return this.context.Etudiants
+            .Include(e => e.Courses)
+            .ToList();
+    }
+
+    public List<Etudiant> Search (string criterion)
+    {
+        return this.context.Etudiants
+                .Include(e => e.Courses)
+                .Where(e =>
+                     e.FirstName.Contains(criterion)
+                     || e.LastName.Contains(criterion))
+                .ToList();
+    }
+
+    public Etudiant Insert(Etudiant etudiant)
+    {
+        this.context.Etudiants.Add(etudiant);
+        this.context.SaveChanges();
+        return etudiant;
+    }
+
+    public Etudiant Update(Etudiant etudiant)
+    {
+        this.context.Etudiants.Update(etudiant);
+        this.context.SaveChanges();
+        return etudiant;
+    }
+
+    public Etudiant Delete(Etudiant etudiant)
+    {
+        this.context.Etudiants.Remove(etudiant);
+        this.context.SaveChanges();
+        return etudiant;
+    }
+}
