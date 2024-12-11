c617f42daa96a9e5642a3cdff52124f99ab157dd
P-S.Cypre <pscypre@gmail.com>
2024-12-11 08:50:48 -0500

    Étape 3 : Configuration des entités ⚙️

---
+++ 420DA3_Examen_Final/DataAccess/AppDbContext.cs
@@ -0,0 +1,144 @@
+﻿using _420DA3_Examen_Final.Business.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.DataAccess;
+internal class AppDbContext : DbContext
+{
+    public DbSet<Etudiant> Etudiants { get; set; }
+    public DbSet<Cours> Courses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        string connString = ConfigurationManager.ConnectionStrings["ProjectDatabase_Final"].ConnectionString;
+        optionsBuilder
+            .UseSqlServer(connString)
+            .UseLazyLoadingProxies();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        _ = modelBuilder.Entity<Etudiant>()
+            .ToTable(nameof(this.Etudiants))
+            .HasKey(e => e.Id);
+
+        _ = modelBuilder.Entity<Cours>()
+            .ToTable(nameof(this.Courses))
+            .HasKey(c => c.Id);
+
+
+        _ = modelBuilder.Entity<Etudiant>()
+            .Property(e => e.Id)
+            .HasColumnType("int")
+            .HasColumnOrder(0)
+            .HasColumnName(nameof(Etudiant.Id))
+            .IsRequired(true)
+            .UseIdentityColumn(1, 1);
+
+        _ = modelBuilder.Entity<Etudiant>()
+            .Property(e => e.FirstName)
+            .HasColumnType($"nvarchar({Etudiant.FirstNameMaxLength})")
+            .HasMaxLength(Etudiant.FirstNameMaxLength)
+            .HasColumnOrder(1)
+            .HasColumnName(nameof(Etudiant.FirstName))
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<Etudiant>()
+            .Property(e => e.LastName)
+            .HasColumnType($"nvarchar({Etudiant.LastNameMaxLength})")
+            .HasMaxLength(Etudiant.LastNameMaxLength)
+            .HasColumnOrder(2)
+            .HasColumnName(nameof(Etudiant.LastName))
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<Etudiant>()
+            .Property(e => e.RowVersion)
+            .HasColumnOrder(3)
+            .HasColumnName(nameof(Etudiant.RowVersion))
+            .IsRequired(true)
+            .IsRowVersion();
+
+
+
+
+        _ = modelBuilder.Entity<Cours>()
+            .Property(c => c.Id)
+            .HasColumnType("int")
+            .HasColumnOrder(0)
+            .HasColumnName(nameof(Cours.Id))
+            .IsRequired(true)
+            .UseIdentityColumn(1, 1);
+
+        _ = modelBuilder.Entity<Cours>()
+            .Property(c => c.CourseCode)
+            .HasColumnType($"nvarchar({Cours.CourseCodeMaxLength})")
+            .HasMaxLength(Cours.CourseCodeMaxLength)
+            .HasColumnOrder(1)
+            .HasColumnName(nameof(Cours.CourseCode))
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<Cours>()
+            .Property(c => c.CourseTitle)
+            .HasColumnType($"nvarchar({Cours.CourseTitleMaxLength})")
+            .HasMaxLength(Cours.CourseTitleMaxLength)
+            .HasColumnOrder(2)
+            .HasColumnName(nameof(Cours.CourseTitle))
+            .IsRequired(true);
+
+        _ = modelBuilder.Entity<Cours>()
+            .Property(c => c.RowVersion)
+            .HasColumnOrder(3)
+            .HasColumnName(nameof(Cours.RowVersion))
+            .IsRequired(true)
+            .IsRowVersion();
+
+
+        _ = modelBuilder.Entity<Cours>()
+            .HasData(
+                new Cours { Id = 1, CourseCode = "420DA3AS ", CourseTitle = "Applications Multi-Tiers" },
+                new Cours { Id = 2, CourseCode = "420DW3AS", CourseTitle = "Web Serveur I" },
+                new Cours { Id = 3, CourseCode = "420PA3AS ", CourseTitle = "Orienté-Objet Avancé" }
+            );
+
+        _ = modelBuilder.Entity<Etudiant>()
+            .HasData(
+                new Etudiant { Id = 1, FirstName = "John", LastName = "Doe" },
+                new Etudiant { Id = 2, FirstName = "Jane", LastName = "Doe" }
+            );
+
+
+        _ = modelBuilder.Entity<Etudiant>()
+            .HasMany(e => e.Courses)
+            .WithMany(c => c.EtudiantInscrits)
+            .UsingEntity(
+                "CoursEtudiant",
+                right =>
+                {
+                    return right.HasOne(typeof(Cours)).WithMany().HasForeignKey("CoursId").HasPrincipalKey(nameof(Cours.Id));
+                },
+                left =>
+                {
+                    return left.HasOne(typeof(Etudiant)).WithMany().HasForeignKey("EtudiantId").HasPrincipalKey(nameof(Etudiant.Id));
+                },
+                config =>
+                {
+                    config.ToTable("CoursEtudiants").HasKey("EtudiantId", "CoursId");
+                    config.HasData(
+                            new { EtudiantId = 1, CoursId = 1 },
+                            new { EtudiantId = 2, CoursId = 2 }
+                        );
+                });
+    }
+
+
+}
---
+++ 420DA3_Examen_Final/Migrations/20241211134837_InitialCreate.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace _420DA3_Examen_Final.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseCode = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    CourseTitle = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Etudiants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etudiants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CoursEtudiants",
+                columns: table => new
+                {
+                    EtudiantId = table.Column<int>(type: "int", nullable: false),
+                    CoursId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CoursEtudiants", x => new { x.EtudiantId, x.CoursId });
+                    table.ForeignKey(
+                        name: "FK_CoursEtudiants_Courses_CoursId",
+                        column: x => x.CoursId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CoursEtudiants_Etudiants_EtudiantId",
+                        column: x => x.EtudiantId,
+                        principalTable: "Etudiants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "CourseCode", "CourseTitle" },
+                values: new object[,]
+                {
+                    { 1, "420DA3AS ", "Applications Multi-Tiers" },
+                    { 2, "420DW3AS", "Web Serveur I" },
+                    { 3, "420PA3AS ", "Orienté-Objet Avancé" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Etudiants",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "John", "Doe" },
+                    { 2, "Jane", "Doe" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "CoursEtudiants",
+                columns: new[] { "CoursId", "EtudiantId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CoursEtudiants_CoursId",
+                table: "CoursEtudiants",
+                column: "CoursId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CoursEtudiants");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Etudiants");
+        }
+    }
+}
