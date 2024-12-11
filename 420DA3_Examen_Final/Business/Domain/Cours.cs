0a4a408cc1194610fcf52b494c8ebc8535d13f2e
P-S.Cypre <pscypre@gmail.com>
2024-12-11 08:26:29 -0500

    Étape 2: Développement de classes du domaine / entités 📕

---
+++ 420DA3_Examen_Final/Business/Domain/Cours.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.Business.Domain;
+public class Cours
+{
+    public const int CourseCodeMaxLength = 16;
+    public const int CourseTitleMaxLength = 256;
+
+    private string courseCode = null!;
+    private string courseTitle = null!;
+
+    public int Id { get; set; }
+    public string CourseCode
+    {
+        get { return this.courseCode; }
+        set
+        {
+            if (value.Length > CourseCodeMaxLength)
+            {
+                throw new ArgumentException(nameof(this.CourseCode), $"Le code de cours ne peut dépasser {CourseCodeMaxLength} caractères.");
+            }
+            this.courseCode = value;
+        }
+    }
+    public string CourseTitle
+    {
+        get { return this.courseTitle; }
+        set
+        {
+            if (value.Length > CourseTitleMaxLength)
+            {
+                throw new ArgumentException(nameof(this.CourseTitle), $"Le titre de cours ne peut dépasser {CourseTitleMaxLength} caractères.");
+            }
+            this.courseTitle = value;
+        }
+    }
+    public byte[] RowVersion { get; set; } = null!;
+
+    public virtual List<Etudiant> EtudiantInscrits { get; set; } = new List<Etudiant>();
+
+    public Cours()
+    {
+
+    }
+
+    protected Cours(int id, string courseCode, string courseTitle, byte[] rowVersion)
+    {
+        this.Id = id;
+        this.CourseCode = courseCode;
+        this.CourseTitle = courseTitle;
+        this.RowVersion = rowVersion;
+    }
+
+    public override string ToString()
+    {
+        return $"#{this.Id} - {this.CourseCode} ({this.CourseTitle})";
+    }
+
+}
---
+++ 420DA3_Examen_Final/Business/Domain/Etudiant.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Examen_Final.Business.Domain;
+public class Etudiant
+{
+    public const int FirstNameMaxLength = 64;
+    public const int LastNameMaxLength = 64;
+
+    private string firstName = null!;
+    private string lastName = null!;
+
+    public int Id { get; set; }
+    public string FirstName
+    {
+        get { return this.firstName; }
+        set
+        {
+            if (value.Length > FirstNameMaxLength)
+            {
+                throw new ArgumentException(nameof(this.FirstName), $"Le prénom ne peut dépasser {FirstNameMaxLength} caractères.");
+            }
+            this.firstName = value;
+        }
+    }
+    public string LastName
+    {
+        get { return this.lastName; }
+        set
+        {
+            if (value.Length > LastNameMaxLength)
+            {
+                throw new ArgumentException(nameof(this.LastName), $"Le nom de famille ne peut dépasser {LastNameMaxLength} caractères.");
+            }
+            this.lastName = value;
+        }
+    }
+    public byte[] RowVersion { get; set; } = null!;
+
+    public virtual List<Cours> Courses { get; set; } = new List<Cours>();
+
+    public Etudiant()
+    {
+
+    }
+
+    protected Etudiant(int id, string firstName, string lastName, byte[] rowVersion)
+    {
+        this.Id = id;
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.RowVersion = rowVersion;
+    }
+
+    public override string ToString()
+    {
+        return $"#{this.Id} - {this.FirstName} ({this.LastName})";
+    }
+}
